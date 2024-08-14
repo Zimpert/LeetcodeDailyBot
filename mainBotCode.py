a0af4607e9c833bbb939bffb17389c3da0f65113
IbrahAbd <ibrahim.abdou2121@gmail.com>
2024-08-14 14:33:56 +0200

    Added main bot code.

---
+++ mainBotCode.py
@@ -0,0 +1,9 @@
+from typing import Final
+from discord import Intents,Client,Message
+from discord.ext import tasks, commands
+from dotenv import load_dotenv
+import asyncio
+import os
+import requests
+import math
+import discord
